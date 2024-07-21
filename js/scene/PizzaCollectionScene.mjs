8583c99bc571cd29e1e3c56beaec5ba959da7718
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2024-07-21 16:46:36 +0900

    feat: pizzas.mjsの内容をPizzaCollectionSceneで表示
    Co-authored-by: yuiyyui <yuiyyui@users.noreply.github.com>

--- js/scene/PizzaCollectionScene.mjs
+++ js/scene/PizzaCollectionScene.mjs
@@ -1,6 +1,7 @@
 import { Scene } from './special/Scene.mjs';
 import { scenes } from "./special/sceneSettings.mjs";
-import { resource } from '../resource.mjs';
+import { pizzaName, recipe, pizzaOrder, imageForPizza } from '../gameObject/pizzas.mjs';
+import { imageForIngredient } from '../gameObject/ingredients.mjs';
 
 // ピザコレクション画面
 export class PizzaCollectionScene extends Scene {
@@ -9,21 +10,7 @@ export class PizzaCollectionScene extends Scene {
         this.nextPageButtonArea = null;
         this.previousPageButtonArea = null;
         this.page = 1; //ページ数
-
-        this.pizzaImages = { // ピザの画像
-            margherita: resource.images.margherita,
-            marinara: resource.images.marinara,
-            seafood: resource.images.seafood
-        }
-
-        this.ingredientImages = { //材料の画像
-            cheese: resource.images.cheese,
-            basil: resource.images.basil,
-            octopus: resource.images.octopus,
-            tomato: resource.images.tomato,
-            onion: resource.images.onion,
-            squid: resource.images.squid,
-        };
+        this.pizzaFrame = [];
     }
 
     updateStates(deltaTime) {}
@@ -32,10 +19,10 @@ export class PizzaCollectionScene extends Scene {
         const max_x = ctx.canvas.width;
         const max_y = ctx.canvas.height;
 
-        //ピザと材料の画像を配置する枠の位置
-        //計算で出した値を使うように修正すべき
-        const pizzaFrame = [{x:25, y:100}, {x:25, y:200}, {x:25, y:300}, {x:25, y:400},
-                            {x:425, y:100}, {x:425, y:200}, {x:425, y:300}, {x:425, y:400} ]
+        // ピザと材料の画像を配置する枠の位置
+        // TODO: 計算で出した値を使うように修正すべき
+        this.pizzaFrame = [{x:25, y:100}, {x:25, y:200}, {x:25, y:300}, {x:25, y:400},
+                            {x:425, y:100}, {x:425, y:200}, {x:425, y:300}, {x:425, y:400}];
 
         ctx.fillStyle = "pink";
         ctx.fillRect(0, 0, max_x, max_y);
@@ -55,27 +42,9 @@ export class PizzaCollectionScene extends Scene {
         ctx.textBaseline = "middle";
         ctx.fillText("タイトルに戻る", r.x + r.w / 2, r.y + r.h / 2);
 
+        this.renderPage(ctx, this.page);
 
-        // 1ページ目
-        if(this.page === 1){
-
-            // sharedDataの形式に合わせて後ほど修正
-            const pizzasPage1 = [
-                { name: 'margherita', displayName:'マルゲリータ', ingredients: ['cheese', 'tomato', 'onion', 'basil'] },
-                { name: 'marinara', displayName:'マリナーラ', ingredients: ['basil', 'octopus', 'cheese', 'squid'] },
-                { name: 'seafood', displayName:'シーフード', ingredients: ['basil', 'octopus', 'cheese', 'squid'] },
-                { name: 'margherita', displayName:'マルゲリータ', ingredients: ['cheese', 'tomato', 'onion', 'basil'] },
-                { name: 'marinara', displayName:'マリナーラ', ingredients: ['basil', 'octopus', 'cheese', 'squid'] },
-                { name: 'marinara', displayName:'マリナーラ', ingredients: ['basil', 'octopus', 'cheese', 'squid'] },
-                { name: 'margherita', displayName:'マルゲリータ', ingredients: ['cheese', 'tomato', 'onion', 'basil'] },
-                { name: 'seafood', displayName:'シーフード', ingredients: ['cheese', 'cheese', 'cheese', 'cheese'] }
-            ];
-
-            for (let i = 0; i < 8; i++) {
-                const pizza = pizzasPage1[i];
-                this.drawPizzaAndIngredients(ctx, pizza.name, pizza.displayName, pizza.ingredients, pizzaFrame[i].x, pizzaFrame[i].y);
-            }
-
+        if (pizzaOrder.length - this.pizzaFrame.length * this.page > 0) {
             // 次のページに遷移するボタン
             r = { x: max_x - 50, y: max_y - 100, w: 50, h: 50 }
             this.nextPageButtonArea = r;
@@ -86,28 +55,10 @@ export class PizzaCollectionScene extends Scene {
             ctx.textAlign = "center";
             ctx.textBaseline = "middle";
             ctx.fillText("→", r.x + r.w / 2, r.y + r.h / 2);
+        } else {
+            this.nextPageButtonArea = null;
         }
-
-        //2ページ目
-        if(this.page === 2){
-
-            const pizzasPage2 = [
-                { name: 'margherita', displayName:'マルゲリータ', ingredients: ['cheese', 'tomato', 'onion', 'basil'] },
-                { name: 'margherita', displayName:'マルゲリータ', ingredients: ['cheese', 'tomato', 'onion', 'basil'] },
-                { name: 'margherita', displayName:'マルゲリータ', ingredients: ['cheese', 'tomato', 'onion', 'basil'] },
-                { name: 'marinara', displayName:'マリナーラ', ingredients: ['basil', 'octopus', 'cheese', 'squid'] },
-                { name: 'marinara', displayName:'マリナーラ', ingredients: ['basil', 'octopus', 'cheese', 'squid'] },
-                { name: 'seafood', displayName:'シーフード', ingredients: ['basil', 'octopus', 'cheese', 'squid'] },
-                { name: 'marinara', displayName:'マリナーラ', ingredients: ['basil', 'octopus', 'cheese', 'squid'] },
-                { name: 'seafood', displayName:'シーフード', ingredients: ['cheese', 'cheese', 'cheese', 'cheese'] }
-            ];
-
-            for (let i = 0; i < 8; i++) {
-                const pizza = pizzasPage2[i];
-                this.drawPizzaAndIngredients(ctx, pizza.name, pizza.displayName,pizza.ingredients, pizzaFrame[i].x, pizzaFrame[i].y);
-            }
-
-
+        if (this.page > 1) {
             // 前のページに遷移するボタン
             r = { x: 0, y: max_y - 100, w: 50, h: 50 }
             this.previousPageButtonArea = r;
@@ -118,9 +69,18 @@ export class PizzaCollectionScene extends Scene {
             ctx.textAlign = "center";
             ctx.textBaseline = "middle";
             ctx.fillText("←", r.x + r.w / 2, r.y + r.h / 2);
+        } else {
+            this.previousPageButtonArea = null;
         }
+    }
 
-
+    renderPage(ctx, page) {
+        for (let i = 0; i < this.pizzaFrame.length; i++) {
+            const pizzaIndex = this.pizzaFrame.length * (page - 1) + i;
+            if (pizzaIndex >= pizzaOrder.length) break;
+            const pizza = pizzaOrder[pizzaIndex];
+            this.drawPizzaAndIngredients(ctx, pizza, this.pizzaFrame[i].x, this.pizzaFrame[i].y);
+        }
     }
 
     // 画面内のどこかがタップされた
@@ -140,7 +100,7 @@ export class PizzaCollectionScene extends Scene {
     }
 
     // ピザの画像とピザ名と材料の画像を描画
-    drawPizzaAndIngredients(ctx, pizzaName, displayName, ingredients, x, y) {
+    drawPizzaAndIngredients(ctx, pizza, x, y) {
         ctx.fillStyle = "white";
         ctx.fillRect(x, y, 350, 90);
 
@@ -150,25 +110,22 @@ export class PizzaCollectionScene extends Scene {
         ctx.fillStyle = 'black';
         ctx.font = "30px Arial";
         ctx.textAlign = "left";
-        ctx.fillText(displayName, x + 100, y + 25);
+        ctx.fillText(pizzaName[pizza], x + 100, y + 25);
 
         // ピザ画像
-        const pizzaImage = this.pizzaImages[pizzaName];
-        if (pizzaImage && pizzaImage.complete) {
-            ctx.imageSmoothingEnabled = false;
-            ctx.drawImage(pizzaImage, x, y, 90, 90);
-        }
+        const pizzaImage = imageForPizza(pizza);
+        ctx.imageSmoothingEnabled = false;
+        ctx.drawImage(pizzaImage, x, y, 90, 90);
 
         // 材料画像
-        ingredients.forEach((ingredient, index) => {
-            const ingredientImage = this.ingredientImages[ingredient];
-            if (ingredientImage && ingredientImage.complete) {
-                ctx.imageSmoothingEnabled = false;
-                ctx.drawImage(ingredientImage, x + 100 + (index * 60), y + 38, 50, 50);
-            }
-        });
+        const ingredients = recipe[pizza];
+        for (let i = 0; i < ingredients.length; i++) {
+            const ingredientImage = imageForIngredient(ingredients[i]);
+            ctx.imageSmoothingEnabled = false;
+            ctx.drawImage(ingredientImage, x + 100 + (i * 60), y + 38, 50, 50);
+        }
     }
-    
+
     // 「タイトルに戻る」ボタンがタップされた
     didTapBack() {
         this.sceneRouter.changeScene(scenes.title);
@@ -176,15 +133,12 @@ export class PizzaCollectionScene extends Scene {
 
     // 「→」がタップされた
     didTapNextPage(){
-        this.page = 2;
+        this.page += 1;
 
     }
 
     // 「←」がタップされた．
     didTapPrePage(){
-        this.page = 1;
+        this.page -= 1;
     }
-
-
-
 }

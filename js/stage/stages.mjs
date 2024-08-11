85a256641074b6100b69bb44e839b95ec23c4165
yuiyui <164984141+yuiyyui@users.noreply.github.com>
2024-08-11 13:57:48 +0900

    feat: stage4の実装

--- js/stage/stages.mjs
+++ js/stage/stages.mjs
@@ -1,9 +1,12 @@
 import { stage1 } from "./stage1.mjs";
 import { stage3 } from "./stage3.mjs";
+import { stage4 } from "./stage4.mjs";
 import { stage5 } from "./stage5.mjs"; 
+
 
 export const stages = {
     1: stage1,
     3: stage3,
+    4: stage4,
     5: stage5,
 }
